c27a3c2424bb2afed5dc66925aeb92f3581bddaf
Teja Reddy <76819847+Tejareddy4@users.noreply.github.com>
2025-09-05 13:10:32 +0530

    Initial Setup

---
+++ src/main/java/in/tejareddy/todosimple/TodoSimpleApplication.java
@@ -0,0 +1,13 @@
+package in.tejareddy.todosimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/tejareddy/todosimple/TodoSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package in.tejareddy.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
